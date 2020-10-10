7b5d059a691a12de379eefb57822c5568749700f
zarazan <zarazan@gmail.com>
2020-10-10 08:40:16 -0600

    add react client

---
+++ client/src/setupProxy.js
@@ -0,0 +1,10 @@
+import { createProxyMiddleware } from 'http-proxy-middleware';
+export default function(app) {
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: 'http://localhost:3001',
+            changeOrigin: true,
+        })
+    );
+};
