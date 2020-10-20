d4260223b1d813da592eab9944bcdea62a587d91
Troy <troy.c.jennings@gmail.com>
2020-10-20 17:43:07 -0600

    [feat] Implement project create scaffolding

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
+import PoolCreate from './components/PoolCreate';
 
 function App() {
 
   return (
     <div className="App">
-      <Dashboard/>
+      <PoolCreate/>
     </div>
   );
 }
