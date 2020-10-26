81d5d4a1706ac7aca44f2064ae537b6348ffbe97
Troy <troy.c.jennings@gmail.com>
2020-10-25 23:04:55 -0600

    [feat] Implements topbar navigation scaffolding
    * Refactor App to grid with persistent
    * Add link icons with defaulted routes
    * Add some "sex" to the nav styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,93 @@
 import React from 'react';
+import { BrowserRouter as Router, NavLink } from 'react-router-dom';
 import Routes from './Routes';
+import styled from 'styled-components';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faChartLine, faTrophy, faHome } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
-    <div className="App">
-      <Routes />
-    </div>
+    <AppWrapper className='app'>
+      <Router>
+        <AppNavigation>
+          <StyledLink>
+            <NavLink to='/' className='home'>
+              <FontAwesomeIcon icon={faHome} size='lg' />
+            </NavLink>
+            <span>Home</span>
+          </StyledLink>
+          <StyledLink>
+            <NavLink to='/' className='user-pools'>
+              <FontAwesomeIcon icon={faTrophy} size='lg' />
+            </NavLink>
+            <span>Pools</span>
+          </StyledLink>
+          <StyledLink>
+            <NavLink to='/' className='user-statistics'>
+              <FontAwesomeIcon icon={faChartLine} size='lg' />
+            </NavLink>
+            <span>Stats</span>
+          </StyledLink>
+          <StyledLink>
+            <NavLink to='/' className='user-profile'>
+              <FontAwesomeIcon icon={faUser} size='lg' />
+            </NavLink>
+            <span>Profile</span>
+          </StyledLink>
+        </AppNavigation>
+        <AppMain>
+          <Routes />
+        </AppMain>
+      </Router>
+    </AppWrapper>
   );
 }
 
 export default App;
+
+const AppWrapper = styled.div`
+  display: grid;
+  grid-template-rows: 4em auto;
+  grid-template-areas:
+    'nav'
+    'main';
+  height: 100vh;
+  box-sizing: border-box;
+`;
+
+const AppNavigation = styled.nav`
+  grid-area: nav;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-evenly;
+  padding: 0.75em;
+
+  background: #f5f5f5;
+  border-bottom: 1px solid #cccccc;
+`;
+
+const AppMain = styled.main`
+  grid-area: main;
+`;
+
+const StyledLink = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+
+  & span {
+    margin-top: 0.30em;
+    font-size: 0.75em;
+    color: #b3b3b3;
+  }
+
+  & a {
+    color: #b3b3b3;
+  }
+
+  & a:hover {
+    color: #560bad;
+  }
+`;
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,50 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
 import MyPools from './MyPools';
 
-const Dashboard = props => {
+const Dashboard = () => {
     return (
-        <PageWrapper>
-            <HeaderWrapper>
-                <Welcome/>
-                <FontAwesomeIcon icon={faUser} size="2x"/>
-            </HeaderWrapper>
+        <DashboardWrapper>
             <StatsWrapper>
                 {'future stats'}
             </StatsWrapper>
             <MainWrapper>
                 <MyPools />
             </MainWrapper>
-        </PageWrapper>
+        </DashboardWrapper>
     );
 }
 
 export default Dashboard;
 
-const PageWrapper = styled.div`
+const DashboardWrapper = styled.div`
     display: grid;
     grid-template-columns: 1fr;
-    grid-template-rows: 3em 10em auto;
+    grid-template-rows: 3em 10em auto 3em;
     grid-template-areas:
-        "header"
+        "top-nav"
         "stats"
         "main";
     height: 100vh;
-    box-sizing: border-box;
-`;
-
-const HeaderWrapper = styled.header`
-    grid-area: "header";
-    
-    display: flex;
-    flex-flow: row nowrap;
-    justify-content: space-between;
-    align-content: space-between;
-    align-items: flex-start;
-
-    margin: 2em;
     box-sizing: border-box;
 `;
 
@@ -56,8 +37,4 @@ const StatsWrapper = styled.section`
 
 const MainWrapper = styled.main`
     grid-area: "main"
-`;
-
-const Welcome = styled.div`
-    font-size: 24px;
 `;
