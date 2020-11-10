80479e480d9083fc0695e2a8a9dd971dc4a43e11
Troy <troy.c.jennings@gmail.com>
2020-11-10 14:19:35 -0700

    [feat] Implements proper bet slip state
    * Adds function for processing odds data into state
    * Handles the rendering of specific bets into specific buttons
    * Adds a new BetButtonOverUnder button type for displaying O/U labels

--- client/src/components/betslip/BetCard.js
+++ client/src/components/betslip/BetCard.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import BetButton from '../../stories/BetButton';
+import BetButtonOverUnder from '../../stories/BetButtonOverUnder';
 import { decToAmerican, zuluToStringFormat } from '../../utilities/helpers';
 
-const BetCard = ({ selectBet, homeTeamName, homeTeamId, awayTeamName, awayTeamId, odds, gameDate }) => {    
+const BetCard = ({ selectBet, homeTeamName, homeTeamId, awayTeamName, awayTeamId, odds, gameDate }) => {
+    const [homeOdds, setHomeOdds] = useState({ 'pointSpread': { 'betId': null, 'metric': null, 'ratio': null }, 'moneyLine': { 'betId': null, 'metric': null, 'ratio': null }, 'totalPoints': { 'betId': null, 'metric': null, 'ratio': null } });
+    const [awayOdds, setAwayOdds] = useState({ 'pointSpread': { 'betId': null, 'metric': null, 'ratio': null }, 'moneyLine': { 'betId': null, 'metric': null, 'ratio': null }, 'totalPoints': { 'betId': null, 'metric': null, 'ratio': null } });
+    
+    useEffect(() => {
+        // seed state
+        let home = createSeedData(odds, 'home', homeTeamId);
+        let away = createSeedData(odds, 'away', awayTeamId);
+        setHomeOdds(home);
+        setAwayOdds(away);
+    }, []);
+
     return (
         <BetCardWrapper className='bet-card'>
             <div className='bet-card__headers'>
@@ -13,37 +25,96 @@ const BetCard = ({ selectBet, homeTeamName, homeTeamId, awayTeamName, awayTeamId
             <div className='card-row bet-card__home'>
                 <Team className='team-home'>{homeTeamName}</Team>
                     <OddsContainer className='odds-container'>
-                        {odds
-                            .filter(odd => odd.teamId === homeTeamId)
-                            .map((odd, index) => (
-                                <BetButton
-                                    key={index}
-                                    className='odd money-line'
-                                    metric={odd.metric}
-                                    ratio={decToAmerican(odd.ratio)}
-                                    callback={() => selectBet(odd.fixtureId, odd.id)}
-                                />
-                            ))}
+
+                        <BetButton 
+                            key={'homeSpread'}
+                            className='odd spread'
+                            metric={homeOdds.pointSpread.metric}
+                            ratio={decToAmerican(homeOdds.pointSpread.ratio)}
+                            callback={() => selectBet(odds.fixtureId, homeOdds.pointSpread.betId)}
+                        />
+                        <BetButtonOverUnder 
+                            key={'homeTotal'}
+                            className='odd total'
+                            which={'over'}
+                            metric={homeOdds.totalPoints.metric}
+                            ratio={decToAmerican(homeOdds.totalPoints.ratio)}
+                            callback={() => selectBet(odds.fixtureId, homeOdds.totalPoints.betId)}
+                        />
+                        <BetButton 
+                            key={'homeMoney'}
+                            className='odd money'
+                            metric={homeOdds.moneyLine.metric}
+                            ratio={decToAmerican(homeOdds.moneyLine.ratio)}
+                            callback={() => selectBet(odds.fixtureId, homeOdds.moneyLine.betId)}
+                        />
+
                     </OddsContainer>
             </div>
             <div className='card-row bet-card__away'>
                 <Team className='team-away'>{awayTeamName}</Team>
                 <OddsContainer className='odds-container'>
-                    {odds
-                        .filter(odd => odd.teamId === awayTeamId)
-                        .map((odd, index) => (
-                            <BetButton
-                                key={index}
-                                className='odd money-line'
-                                metric={odd.metric}
-                                ratio={decToAmerican(odd.ratio)}
-                                callback={() => selectBet(odd.fixtureId, odd.id)}
-                            />
-                        ))}
+
+                    <BetButton 
+                        key={'awaySpread'}
+                        className='odd spread'
+                        metric={awayOdds.pointSpread.metric}
+                        ratio={decToAmerican(awayOdds.pointSpread.ratio)}
+                        callback={() => selectBet(odds.fixtureId, awayOdds.pointSpread.betId)}
+                    />
+                    <BetButtonOverUnder 
+                        key={'awayTotal'}
+                        className='odd total'
+                        which={'under'}
+                        metric={awayOdds.totalPoints.metric}
+                        ratio={decToAmerican(awayOdds.totalPoints.ratio)}
+                        callback={() => selectBet(odds.fixtureId, awayOdds.totalPoints.betId)}
+                    />
+                    <BetButton 
+                        key={'awayMoney'}
+                        className='odd money'
+                        metric={awayOdds.moneyLine.metric}
+                        ratio={decToAmerican(awayOdds.moneyLine.ratio)}
+                        callback={() => selectBet(odds.fixtureId, awayOdds.moneyLine.betId)}
+                    />
+
                 </OddsContainer>
             </div>
         </BetCardWrapper>
     );
+
+    /** createSeedData: Filters the fixture data and returns the neccessary shape for local bet state. */
+    function createSeedData(oddsData, teamType, teamId) {
+        let newState = {};
+        let totalPoints = { 'betId': null, 'metric': null, 'ratio': null };
+  
+        // get arrays of odds by home vs. away
+        const teamOdds = oddsData.filter(odd => odd.teamId === teamId);
+       
+        // get each bet type
+        const spreadBet = teamOdds.filter(d => d.teamId === teamId && d.type === 'spread').pop();
+        const moneyBet = teamOdds.filter(d => d.teamId === teamId && d.type === 'money_line').pop();
+        const over = oddsData.filter(d => d.type === 'over').pop();
+        const under = oddsData.filter(d => d.type === 'under').pop();
+        
+        // seed team-based data
+        newState.moneyLine = { 'betId': moneyBet.id, 'metric': '', 'ratio': moneyBet.ratio };
+        newState.pointSpread = { 'betId': spreadBet.id, 'metric': spreadBet.metric, 'ratio': spreadBet.ratio };
+        
+        if(teamType === 'home') {            
+            totalPoints.betId = over.id;
+            totalPoints.metric = over.metric;
+            totalPoints.ratio = over.ratio;
+            newState.totalPoints = totalPoints;
+
+        } else {
+            totalPoints.betId = under.id;
+            totalPoints.metric = under.metric;
+            totalPoints.ratio = under.ratio;
+            newState.totalPoints = totalPoints;
+        }
+        return newState;
+    }
 };
 
 BetCard.propTypes = {
---
+++ client/src/stories/BetButtonOverUnder.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const BetButton = ({ className, which, metric, ratio, callback }) => {
+    const [selected, setSelected] = useState(false);
+
+    return (
+        <Odd className={`${className} ${selected ? 'selected' : ''}`}>
+            <ToggleButton
+                onClick={toggleSelected}
+            >
+                <div>
+                    <span>
+                        {metric 
+                        ? which === 'over'
+                            ? `O ${metric}`
+                            : `U ${metric}`
+                        : ''}
+                    </span>
+                    <span>
+                        {ratio > 0
+                        ? `+${ratio}`
+                        : ratio
+                        }
+                    </span>
+                </div>
+                
+            </ToggleButton>
+        </Odd>
+    );
+
+    function toggleSelected() {
+        if(selected === false) {
+            callback();
+        }
+        setSelected(!selected);
+    }
+};
+
+BetButton.propTypes = {
+    className: PropTypes.string,
+    value: PropTypes.string.isRequired,
+};
+
+export default BetButton;
+
+const Odd = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0;
+
+    &.selected > button {
+        border: 2px solid #eaf3fd;
+        background-color: #2e8dfe;
+        color: white;
+    }
+`;
+
+const ToggleButton = styled.button`
+    box-sizing: border-box;
+    height: 100%;
+    width: 100%;
+    font-family: 'Poppins', 'Sans Serif';
+    font-size: 0.7rem;
+
+    background-color: #f2f2f2;
+    color: black;
+    border-radius: 0.2em;
+    outline: none;
+    border: none;
+
+    & > div {
+        display: flex;
+        flex-flow: column nowrap;
+        justify-content: center;
+        align-items: center;
+        box-sizing: border-box;
+        height: 100%;
+        width: 100%;
+    }
+`;
