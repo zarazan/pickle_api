f4914e6cc0263c5e279641aa33ac2e05f9efaf01
Troy Jennings <troy.c.jennings@gmail.com>
2020-12-13 14:48:57 -0700

    [fix] Adds decimal support and some date formatting

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,7 +9,6 @@ import { UnAuthenticatedApp } from './UnAuthenticatedApp';
 
 const App = () => {
     const [user] = useContext(UserContext);
-
     return (
         <>
             <div className='app' style={{ height: '100vh', width: '100vw', boxSizing: 'border-box' }}>
