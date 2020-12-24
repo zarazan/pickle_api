86679535309d07ec90957164d9fa5d63de8aedd8
zarazan <zarazan@gmail.com>
2020-12-24 15:35:37 -0700

    remove slow responses for testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,8 @@ class ApplicationController < ActionController::API
 
   include DeviseTokenAuth::Concerns::SetUserByToken
 
-  before_action :make_slow
-
   def fallback_index_html
     render :file => 'public/index.html'
-  end
-
-  def make_slow
-    sleep 2
   end
 
 end
